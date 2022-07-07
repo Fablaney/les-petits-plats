334389b658368660fdf16bbdb35b768f5f128daf
Fablaney <fabrmoto@gmail.com>
2022-07-07 16:09:42 +0200

    form

--- scripts/fonctions/searchbar.js
+++ scripts/fonctions/searchbar.js
@@ -5,20 +5,35 @@ export function searchInput()
 {
     // console.log(recipies)
 
+    // je récupere le champ de recherche
     const searchinput = document.querySelector("#searchinput")
+
+    let inputcontent = ""
 
     searchinput.addEventListener('keyup', function()
     {
-        // const input = searchinput.value.toLowerCase()
-        const inputcontent = searchinput.value
+        // je prends le champ de recherche et si il est vide je n'affiche pas la div de filtre texte actif
+        if (document.querySelector("#searchinput").value == "")
+        {
+            document.querySelector("#filtre-input").classList.add("d-none")
+        }
+        // sinon j'affiche la div
+        else
+        {
+            document.querySelector("#filtre-input").classList.remove("d-none")
+        }
 
-        // console.log(inputcontent)
+        // je récupere sa valeur après avoir tapé 1 lettre et je réduit tout en miniscule
+        const inputcontent = searchinput.value.toLowerCase()
 
-        // Je crée un bloc contennat le texte recherché
-        const filterInputDOM = `<div class="btn btn-primary filtre-input">${inputcontent}</div>`
+        // Je crée le texte recherché
+        const filterInputDOM = `${inputcontent} &nbsp; <i class="bi bi-x-circle"></i>`
 
-        // J'insere ce bloc dans la zone HTML qui affiche els filtres actifs
-        document.querySelector(".filtres-actifs").innerHTML = filterInputDOM
+        // J'insere le texte dans sa div dans la zone HTML qui affiche les filtres actifs
+        document.querySelector("#filtre-input").innerHTML = filterInputDOM
+
+        
     })
 }
 searchInput()
+
