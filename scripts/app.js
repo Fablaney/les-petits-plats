54ab657d193f3d82ee526ad035e7ca80ebf7c47c
Fablaney <fabrmoto@gmail.com>
2022-07-08 15:38:55 +0200

    modern

--- scripts/app.js
+++ scripts/app.js
@@ -7,15 +7,10 @@ import {recipeCardsFactorie} from "/scripts/fonctions/recipecardsfactorie.js";
 import {searchInput} from "/scripts/fonctions/searchbar.js";
 
 
-// affichage de toutes les recettes
-
-let recettesAll = recipies
-let recette = []
-
 // Boucle sur les données
 async function init()
 {
-    recettesAll.forEach((recette) => {
+    recipies.forEach((recette) => {
         recipeCardsFactorie(recette); // Affichage de toutes les recettes au chargement de la page et lors des réinitialisations
     });
 }
