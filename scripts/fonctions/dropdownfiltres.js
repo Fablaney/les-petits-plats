6a7659919fc64bb81e3e93b47f72c17519ab641c
Fablaney <fabrmoto@gmail.com>
2022-07-11 15:24:42 +0200

    modern

---
+++ scripts/fonctions/dropdownfiltres.js
@@ -0,0 +1,8 @@
+// datas
+import {recipies} from "/data/recettes.js"
+
+// creation des articles
+import {recipeCardsFactorie} from "/scripts/fonctions/recipecardsfactorie.js";
+
+
+export function dropdownFilters()
